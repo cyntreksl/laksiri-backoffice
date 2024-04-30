e5f3c961a7dcabb27f35b12d72bd87a9a9993c10
IsuruAhinsa <isurujayawickramat@gmail.com>
2024-04-30 12:17:25 +0530

    create enums

---
+++ app/Enum/UserStatus.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enum;
+
+enum UserStatus: string
+{
+    case ACTIVE = 'ACTIVE';
+    case DEACTIVATE = 'DEACTIVATE';
+    case DELETED = 'DELETED';
+    case INVITED = 'INVITED';
+}
