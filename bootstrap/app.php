89ac25081d42a4959ec0af02e01cdd6e5dca3d3f
IsuruAhinsa <isurujayawickramat@gmail.com>
2024-04-30 12:46:10 +0530

    last logout

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withEvents([
-        __DIR__ . '/../app/Events',
+        __DIR__.'/../app/Events',
     ])
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->web(append: [
