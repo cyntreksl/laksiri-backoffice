d9f19733f011a0001be4f03d36164b36da1fd3f4
Ahinsa <isurujayawickramat@gmail.com>
2024-05-16 15:31:05 +0530

    LAC-58: updated

--- app/Models/HBL.php
+++ app/Models/HBL.php
@@ -7,10 +7,10 @@ use Illuminate\Database\Eloquent\Attributes\ScopedBy;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\Activitylog\LogOptions;
 use Spatie\Activitylog\Traits\LogsActivity;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 #[ScopedBy(BranchScope::class)]
 class HBL extends Model
