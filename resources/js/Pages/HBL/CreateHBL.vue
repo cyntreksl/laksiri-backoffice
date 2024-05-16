584336be00c32a524184c17a5928c29708558394
Ahinsa <isurujayawickramat@gmail.com>
2024-05-16 08:09:36 +0530

    LAC-51: updated

--- resources/js/Pages/HBL/CreateHBL.vue
+++ resources/js/Pages/HBL/CreateHBL.vue
@@ -490,7 +490,7 @@ const calculatePayment = () =>{
                                         class="form-textarea w-full resize-none rounded-lg border border-slate-300 bg-transparent p-2.5 placeholder:text-slate-400/70 hover:border-slate-400 focus:border-primary dark:border-navy-450 dark:hover:border-navy-400 dark:focus:border-accent"
                                     ></textarea>
                                 </label>
-                                <InputError :message="form.errors.consignee_name"/>
+                                <InputError :message="form.errors.consignee_note"/>
                             </div>
                         </div>
                     </div>
