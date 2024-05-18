f54e924dc6c753275b3e5ff348677e188cf4ea3a
Chamathka <miyuridissa@gmail.com>
2024-05-18 13:08:43 +0530

    LAC-76: Create Pickup API

--- app/Traits/ResponseAPI.php
+++ app/Traits/ResponseAPI.php
@@ -8,12 +8,6 @@ trait ResponseAPI
 {
     /**
      * Core of response
-     *
-     * @param string $message
-     * @param object|array|null $data
-     * @param int $statusCode
-     * @param bool $isSuccess
-     * @return JsonResponse
      */
     public function coreResponse(string $message, object|array|null $data, int $statusCode, bool $isSuccess = true): JsonResponse
     {
@@ -33,6 +27,7 @@ trait ResponseAPI
         } else {
             return response()->json([
                 'message' => $message,
+                'errors' => $data,
                 'error' => true,
                 'code' => $statusCode,
             ], $statusCode);
@@ -41,11 +36,6 @@ trait ResponseAPI
 
     /**
      * Send any success response
-     *
-     * @param string $message
-     * @param object|array $data
-     * @param int $statusCode
-     * @return JsonResponse
      */
     public function success(string $message, object|array $data, int $statusCode = 200): JsonResponse
     {
@@ -54,13 +44,9 @@ trait ResponseAPI
 
     /**
      * Send any error response
-     *
-     * @param string $message
-     * @param int $statusCode
-     * @return JsonResponse
      */
-    public function error(string $message, int $statusCode = 500): JsonResponse
+    public function error(string $message, object|array|null $data = null, int $statusCode = 500): JsonResponse
     {
-        return $this->coreResponse($message, null, $statusCode, false);
+        return $this->coreResponse($message, $data, $statusCode, false);
     }
 }
