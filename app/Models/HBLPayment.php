41cca21621c6256ca3e76cc7ced3a8d918bbc9cf
Ahinsa <isurujayawickramat@gmail.com>
2024-05-21 15:29:22 +0530

    LAC-92: enum added

---
+++ app/Models/HBLPayment.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
+
+class HBLPayment extends Model
+{
+    use HasFactory;
+    use LogsActivity;
+    use SoftDeletes;
+
+    protected $table = 'hbl_payments';
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()->logAll()->logOnlyDirty();
+    }
+}
