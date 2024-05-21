68e5e33353b8c326e1ebec3d3ccd60c3dc5ef445
Ahinsa <isurujayawickramat@gmail.com>
2024-05-21 10:29:36 +0530

    LAC-46: filter drawer scroll issue fixed

--- resources/js/Components/FilterDrawer.vue
+++ resources/js/Components/FilterDrawer.vue
@@ -21,9 +21,9 @@ const close = () => {
              x-transition:enter="ease-out" x-transition:enter-end="opacity-100" x-transition:enter-start="opacity-0"
              x-transition:leave="ease-in" x-transition:leave-end="opacity-0"
              x-transition:leave-start="opacity-100"></div>
-        <div class="fixed right-0 top-0 z-[101] h-full w-72">
+        <div class="fixed right-0 top-0 z-[101] h-full w-72 overflow-y-auto">
             <div
-                class="flex h-full p-5 w-full transform-gpu flex-col bg-white transition-transform duration-200 dark:bg-navy-700 show space-y-4"
+                class="flex h-fit p-5 w-full transform-gpu flex-col bg-white transition-transform duration-200 dark:bg-navy-700 show space-y-4"
                 x-transition:enter="ease-out" x-transition:enter-end="translate-x-0"
                 x-transition:enter-start="translate-x-full" x-transition:leave="ease-in"
                 x-transition:leave-end="translate-x-full" x-transition:leave-start="translate-x-0">
