a382503248ac3ecf636c5d0a850505e0c605252d
Ahinsa <isurujayawickramat@gmail.com>
2024-05-22 17:00:18 +0530

    LAC-45: conflicts

--- app/Repositories/PickupRepository.php
+++ app/Repositories/PickupRepository.php
@@ -44,10 +44,12 @@ class PickupRepository implements GridJsInterface, PickupRepositoryInterface
     {
         $query = PickUp::query();
 
-        if (! empty($search)) {
-            $query->where('reference', 'like', '%'.$search.'%');
-            $query->where('name', 'like', '%'.$search.'%');
-            $query->where('contact_number', 'like', '%'.$search.'%');
+        if (!empty($search)) {
+            $query->where(function($query) use ($search) {
+                $query->where('reference', 'like', '%' . $search . '%')
+                    ->orWhere('name', 'like', '%' . $search . '%')
+                    ->orWhere('contact_number', 'like', '%' . $search . '%');
+            });
         }
 
         //apply filters
