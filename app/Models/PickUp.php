cdae82e2af4669bbbd43bbbc892f81d67ec58fae
Ahinsa <isurujayawickramat@gmail.com>
2024-05-22 16:51:45 +0530

    LAC-45: conflicts

--- app/Models/PickUp.php
+++ app/Models/PickUp.php
@@ -39,6 +39,7 @@ class PickUp extends Model
         'pickup_time_end',
         'is_urgent_pickup',
         'is_from_important_customer',
+        'status',
     ];
 
     public function getActivitylogOptions(): LogOptions
