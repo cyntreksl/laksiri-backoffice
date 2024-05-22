0e60961d8b9aed876ae33dbbc0d018cc1ed115ca
Ahinsa <isurujayawickramat@gmail.com>
2024-05-22 16:55:35 +0530

    LAC-45: conflicts

--- app/Repositories/PickupRepository.php
+++ app/Repositories/PickupRepository.php
@@ -7,13 +7,11 @@ use App\Actions\PickUps\AssignDriver;
 use App\Actions\PickUps\CreatePickUp;
 use App\Actions\PickUps\GetPickups;
 use App\Actions\PickUps\GetTotalPickupCount;
-use App\Actions\PickUps\SavePickUpOrder;
 use App\Factory\Pickup\FilterFactory;
 use App\Http\Resources\PickupResource;
 use App\Interfaces\GridJsInterface;
 use App\Interfaces\PickupRepositoryInterface;
 use App\Models\PickUp;
-use Illuminate\Http\Request;
 
 class PickupRepository implements GridJsInterface, PickupRepositoryInterface
 {
@@ -44,7 +42,7 @@ class PickupRepository implements GridJsInterface, PickupRepositoryInterface
     {
         $query = PickUp::query();
 
-        if (! empty($search)) {
+        if (!empty($search)) {
             $query->whereAny(['reference', 'name', 'contact_number'], 'like', '%' . $search . '%');
         }
 
@@ -67,5 +65,28 @@ class PickupRepository implements GridJsInterface, PickupRepositoryInterface
                 'lastPage' => ceil($totalRecords / $limit),
             ],
         ]);
+    }
+
+    public function getFilteredPickups(Request $request)
+    {
+        $query = Pickup::query();
+        if ($request->filled('fromDate') || $request->filled('toDate') || $request->filled('driverId')) {
+            FilterFactory::apply($query, ['fromDate' => $request->fromDate, 'toDate' => $request->toDate, 'driverId' => $request->driverId]);
+        } else {
+            // If no filters are provided, return an empty collection
+            $query->whereRaw('1 = 0');
+        }
+
+        return $query
+            ->with('zone')
+            ->orderBy('pickup_order')
+            ->get();
+    }
+
+    public function savePickupOrder(array $pickups): void
+    {
+        foreach ($pickups as $item) {
+            SavePickUpOrder::run($item);
+        }
     }
 }
