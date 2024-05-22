f9e8023286e6638d5cd2eee33bb13eacd5f4420b
Ahinsa <isurujayawickramat@gmail.com>
2024-05-22 17:12:57 +0530

    LAC-45: conflicts

--- routes/web.php
+++ routes/web.php
@@ -40,11 +40,6 @@ Route::middleware([
     Route::put('pickups/{pickup}/driver/update', [PickupController::class, 'updateDriver'])
         ->name('pickups.driver.update');
 
-    Route::get('pickups/priority/ordering', [PickupController::class, 'showPickupOrder'])
-        ->name('pickups.ordering');
-
-    Route::put('/pickups/update/order', [PickupController::class, 'updatePickupOrder'])
-        ->name('pickups.update-order');
     // HBL
     Route::resource('hbls', HBLController::class);
 
