5ab4031a4ae1fc4e10c9f768cc97e16c694a29dc
Ahinsa <isurujayawickramat@gmail.com>
2024-05-28 14:49:24 +0530

    LAC-103: filters created

---
+++ app/Factory/Container/Filters/EtdStartDateFilter.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Factory\Container\Filters;
+
+use App\Interfaces\FilterInterface;
+use Illuminate\Database\Eloquent\Builder;
+
+class EtdStartDateFilter implements FilterInterface
+{
+    public function apply(Builder $query, $value)
+    {
+        if (! empty($value)) {
+            return $query->whereDate('estimated_time_of_departure', '>=', $value);
+        }
+
+        return $query;
+    }
+}
