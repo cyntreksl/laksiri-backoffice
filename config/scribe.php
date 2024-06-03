72b96e8a82945a80285f271feb0e92f14ca0f79b
Jareer Zeenam <jareerzeenam.28@gmail.com>
2024-06-03 16:38:52 +0100

    LAC-106: Scribe Keys removed

--- config/scribe.php
+++ config/scribe.php
@@ -111,7 +111,7 @@ return [
 
         // The value of the parameter to be used by Scribe to authenticate response calls.
         // This will NOT be included in the generated documentation. If empty, Scribe will use a random value.
-        'use_value' => env('SCRIBE_AUTH_KEY'),
+        'use_value' => 'Bearer {YOUR_AUTH_KEY}',
 
         // Placeholder your users will see for the auth parameter in the example requests.
         // Set this to null if you want Scribe to use a random value as placeholder instead.
