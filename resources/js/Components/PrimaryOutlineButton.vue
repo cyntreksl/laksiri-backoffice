270a7f4ff3487fdeee15a791ec21cd263b269691
Ahinsa <isurujayawickramat@gmail.com>
2024-06-19 14:58:56 +0530

    LAC-152: "add hbl to shipment" button will be activate only container is not loaded

--- resources/js/Components/PrimaryOutlineButton.vue
+++ resources/js/Components/PrimaryOutlineButton.vue
@@ -9,7 +9,7 @@ defineProps({
 
 <template>
     <button :type="type"
-            class="inline-flex items-center btn border border-primary font-medium text-primary hover:bg-primary hover:text-white focus:bg-primary focus:text-white active:bg-primary/90">
+            class="inline-flex items-center btn border border-primary font-medium text-primary hover:bg-primary hover:text-white focus:bg-primary focus:text-white active:bg-primary/90 disabled:pointer-events-none disabled:select-none disabled:opacity-60">
         <slot/>
     </button>
 </template>
