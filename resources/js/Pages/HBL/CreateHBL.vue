28d73376b48cff6fb4bd91e718ec3a627156d4c3
Sudaraka <sudarakarash@gmail.com>
2024-06-26 08:47:39 +0530

    All done

--- resources/js/Pages/HBL/CreateHBL.vue
+++ resources/js/Pages/HBL/CreateHBL.vue
@@ -97,7 +97,9 @@ const editMode = ref(false);
 
 const showPackageDialog = () => {
   showAddNewPackageDialog.value = true;
-  selectedType.value = "";
+  if (!editMode.value) {
+    selectedType.value = "";
+  }
 };
 
 const packageList = ref([]);
@@ -1067,6 +1069,7 @@ const openEditModal = (index) => {
                     @change="updateTypeDescription"
                     class="form-select mt-1.5 w-full rounded-lg border border-slate-300 bg-white px-3 py-2 hover:border-slate-400 focus:border-primary dark:border-navy-450 dark:bg-navy-700 dark:hover:border-navy-400 dark:focus:border-accent"
                   >
+                    <option value="">Choose one</option>
                     <option v-for="type in packageTypes" :key="type">
                       {{ type }}
                     </option>
