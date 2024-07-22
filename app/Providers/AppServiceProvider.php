b21578c188c8044ea77cad7e618ad465d8518dff
Ahinsa <isurujayawickramat@gmail.com>
2024-07-22 08:55:47 +0530

    LAC-196:assigned all those permissions to admin role

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,6 +8,7 @@ use App\Listeners\SetUserCurrentBranch;
 use Illuminate\Auth\Events\Login;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Passport\Passport;
@@ -38,5 +39,11 @@ class AppServiceProvider extends ServiceProvider
         );
 
         Event::listen(Login::class, SetUserCurrentBranch::class);
+
+        // Implicitly grant "Super Admin" role all permissions
+        // This works in the app by using gate-related functions like auth()->user->can() and @can()
+        Gate::before(function ($user, $ability) {
+            return $user->hasRole('admin') ? true : null;
+        });
     }
 }
