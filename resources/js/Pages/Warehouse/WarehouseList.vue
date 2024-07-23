7409c5e436f3badc0e292fa8fec36a5eaac64aed
Ahinsa <isurujayawickramat@gmail.com>
2024-07-23 09:13:17 +0530

    LAC-146: removed unnecessary filter options

--- resources/js/Pages/Warehouse/WarehouseList.vue
+++ resources/js/Pages/Warehouse/WarehouseList.vue
@@ -864,21 +864,6 @@ const shipIcon = ref(`
                     />
                     <div v-html="shipIcon"></div>
                 </label>
-                <label class="inline-flex items-center space-x-2 mt-2">
-                    <Switch
-                        v-model="filters.cargoMode"
-                        label="Sea Cargo"
-                        value="Sea Cargo"
-                    />
-                </label>
-
-                <label class="inline-flex items-center space-x-2 mt-2">
-                    <Switch
-                        v-model="filters.cargoMode"
-                        label="Door to Door"
-                        value="Door to Door"
-                    />
-                </label>
 
                 <FilterBorder/>
 
