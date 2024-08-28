f8ad0fd36ee646547449963a83d7bd8c9218d699
imalshaweerakkodi <imalsha.weerakkodi@cyntrek.com>
2024-08-28 14:01:52 +0530

    LAC-259: Reformat HBL PDF

--- resources/views/pdf/hbls/hbl.blade.php
+++ resources/views/pdf/hbls/hbl.blade.php
@@ -6,79 +6,109 @@
 @endsection
 
 @section('pdf-content')
-    <table>
+    <style>
+        td {
+            font-family: "Roboto", sans-serif;
+            font-size: x-small;
+            padding: 2px;
+        }
+
+        td .hbl-title {
+            color: #636363;
+
+        }
+
+        .page-break {
+            page-break-after: always;
+        }
+    </style>
+    <table style="margin-bottom: -20px;">
         <tbody>
-        <tr>
-            <td style="width: 40%;">
-                <table>
-                    <tbody>
-                    <tr>
-                        <td>Date:</td>
-                        <td>{{ $hbl->created_at->toDateString() }}</td>
-                    </tr>
-                    <tr>
-                        <td>Time:</td>
-                        <td>{{ $hbl->created_at->toTimeString() }}</td>
-                    </tr>
-                    <tr>
-                        <td>Cargo Type:</td>
-                        <td>{{$hbl?->cargo_type}} / {{$hbl?->hbl_type}}</td>
-                    </tr>
-                    <tr>
-                        <td>Final Destination:</td>
-                        <td>{{$hbl?->warehouse}}</td>
-                    </tr>
-                    <tr>
-                        <td>HBL:</td>
-                        <td>{{$hbl?->hbl}}</td>
-                    </tr>
-                    </tbody>
-                </table>
-            </td>
-            <td style="vertical-align: top; width: 60%">
-                <div>
-                    <img style="width: 25%; margin-right: -30px; float: left" src="{{public_path('images/app-logo.png')}}"
-                         alt="app_logo">
-                    <div style="text-align: center; line-height: 1">
-                        <h3 style="text-decoration: underline; margin: 0; padding: 0">DELIVERY AGENT SRI LANKA</h3>
-                        <h4 style="font-weight: bold; margin: 2px 0 0;padding: 0">LAKSIRI SEVA (PVT) LTD</h4>
-                        <p style="margin: 0; padding: 0">NO. 31, ST. ANTHONY'S MW, COLOMBO-03 <br>
-                            TEL: +94 112574180, +94 112575576 | FAX: +94 112575576 <br>
-                            E-mail: laksiriseva@laksirigroup.com | Web: www.laksirigroup.com</p>
+            <tr>
+                <td style="width: 40%;">
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td class="hbl-title">Date:</td>
+                                <td>{{ $hbl->created_at->toDateString() }}</td>
+                            </tr>
+                            <tr>
+                                <td class="hbl-title">Time:</td>
+                                <td>{{ $hbl->created_at->toTimeString() }}</td>
+                            </tr>
+                            <tr>
+                                <td class="hbl-title">Cargo Type:</td>
+                                <td>{{ $hbl?->cargo_type }} / {{ $hbl?->hbl_type }}</td>
+                            </tr>
+                            <tr>
+                                <td class="hbl-title">Final Destination:</td>
+                                <td>{{ $hbl?->warehouse }}</td>
+                            </tr>
+                            <tr>
+                                <td class="hbl-title">HBL:</td>
+                                <td>{{ $hbl?->hbl }}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+                <td style="vertical-align: top; width: 60%">
+                    <div>
+                        <img style="width: 30%; margin-right: -80px; float: left; margin-top: -30px"
+                            src="data:image/png;base64,{{ base64_encode(file_get_contents(public_path('images/app-logo.png'))) }}"
+                            alt="app_logo">
+                        <div style="text-align: center; line-height: 1">
+                            <h4 style="text-decoration: underline; margin: 0; padding: 0">DELIVERY AGENT SRI LANKA</h4>
+                            <h4 style="font-weight: bold; margin: 2px 0 0;padding: 0">LAKSIRI SEVA (PVT) LTD</h4>
+                            <p style="margin: 0; padding: 0; font-family: 'Roboto', sans-serif; color: #636363">NO. 31, ST.
+                                ANTHONY'S MW,
+                                COLOMBO-03 <br>
+                                TEL: +94 112574180, +94 112575576 | FAX: +94 112575576 <br>
+                                E-mail: laksiriseva@laksirigroup.com | Web: www.laksirigroup.com</p>
+                        </div>
                     </div>
-                </div>
-            </td>
-        </tr>
+                </td>
+            </tr>
         </tbody>
     </table>
 
-    <table style="margin-top: 0;">
+    <table style="margin-top: 0; margin-bottom: -16px; width: 100%;">
         <tr>
-            <td rowspan="2" style="width: 50%">
+            <td rowspan="3" style="width: 39%; vertical-align: middle;">
                 @php
                     $generator = new Picqer\Barcode\BarcodeGeneratorPNG();
-echo '<img src="data:image/png;base64,' . base64_encode($generator->getBarcode($hbl->hbl, $generator::TYPE_CODE_128, 3, 80)) . '">';
+                    echo '<img style="width: 90%" src="data:image/png;base64,' .
+                        base64_encode($generator->getBarcode($hbl->hbl, $generator::TYPE_CODE_128, 3, 80)) .
+                        '">';
                 @endphp
             </td>
-            <td colspan="2" style="text-align: center; font-weight: bold; width: 50%">
+            <td colspan="3" style="width: 61%; text-align: center; font-weight: bold; text-decoration: underline;">
                 U.P.B BONDED WAREHOUSE
             </td>
         </tr>
         <tr>
-            <td style="width: 25%">
+            <td rowspan="2" style="width: 18%; vertical-align: top;">
+                <span style="font-weight:bold;">KURUNEGALA</span>
+                <p style="padding-top: 0; margin-top: 0; font-size: xx-small;">
+                    BUS STATION, <br>
+                    ALUTHMALKADUWAWA, <br>
+                    KURUNEGALA. <br>
+                    TEL: +94 112917729
+                </p>
+            </td>
+            <td rowspan="2" style="width: 20%; vertical-align: top;">
                 <span style="font-weight:bold;">COLOMBO</span>
-                <p style="padding-top: 0; margin-top: 0">
+                <p style="padding-top: 0; margin-top: 0; font-size: xx-small;">
                     LAKSIRI SEVA (PVT) LTD, <br>
                     NO. 66, NEW NUGE ROAD, <br>
                     PELIYAGODA. <br>
                     TEL: +94 112917729 / 31
                 </p>
             </td>
-            <td style="width: 25%">
+            <td rowspan="2" style="width: 23%;  vertical-align: top;">
                 <span style="font-weight:bold;">NINTHAVUR</span>
-                <p style="padding-top: 0; margin-top: 0">
+                <p style="padding-top: 0; margin-top: 0; font-size: xx-small;">
                     LAKSIRI SEVA (PVT) LTD, <br>
-                    NO. 26, H.M.Y.L LANE, MAIN STREET <br>
+                    NO. 26, H.M.Y.L LANE, MAIN ST <br>
                     ADDAPPALLAM, NINTHAVUR. <br>
                     TEL: +94 672051050 / 51
                 </p>
@@ -86,7 +116,7 @@ echo '<img src="data:image/png;base64,' . base64_encode($generator->getBarcode($
         </tr>
     </table>
 
-    <table>
+    <table style="margin-top: 0;">
         <tr>
             <td style="border: 1px solid black; border-collapse: collapse; height: 70px">
                 <table class="shipper-table">
@@ -141,73 +171,123 @@ echo '<img src="data:image/png;base64,' . base64_encode($generator->getBarcode($
         </tr>
     </table>
 
-    <table class="package-table">
-        <tr>
-            <th>S/N</th>
-            <th>Description</th>
-            <th>Size</th>
-            <th>QTY</th>
-            <th>Weight</th>
-            <th>Volume</th>
-            <th>Remarks</th>
-        </tr>
-        @forelse($hbl->packages as $package)
-            <tr>
-                <td>{{$loop->iteration}}</td>
-                <td>{{$package?->package_type}}</td>
-                <td>{{$package?->length }} X {{$package?->width }} X {{$package?->height }}</td>
-                <td>{{$package?->quantity}}</td>
-                <td>{{round($package?->weight, 2)}}</td>
-                <td>{{round($package?->volume, 3)}}</td>
-                <td>{{$package?->remarks ?? '-'}}</td>
-            </tr>
-        @empty
-            <tr style="text-align: center">
-                <td colspan="7">No Any Package</td>
-            </tr>
-        @endforelse
-    </table>
+    @php
+        $packagesChunks = collect();
+        $firstPagePackages = $hbl->packages->take(29); // Take first 29 packages for the first page
+        $remainingPackages = $hbl->packages->skip(29); // Skip first 29 packages for remaining pages
 
-    <table>
+        // Add first chunk of 29 packages
+        $packagesChunks->push($firstPagePackages);
+
+        // Split remaining packages into chunks of 10
+        $packagesChunks = $packagesChunks->merge($remainingPackages->chunk(52));
+
+        // Initialize serial number
+        $serialNumber = 1;
+    @endphp
+
+    @foreach ($packagesChunks as $chunk)
+        <table class="package-table">
+            <tr>
+                <th>S/N</th>
+                <th>Description</th>
+                <th>Size</th>
+                <th>QTY</th>
+                <th>Weight</th>
+                <th>Volume</th>
+                <th>Remarks</th>
+            </tr>
+            @forelse($chunk as $package)
+                <tr>
+                    <td>{{ $serialNumber++ }}</td>
+                    <td>{{ $package?->package_type }}</td>
+                    <td>{{ $package?->length }} X {{ $package?->width }} X {{ $package?->height }}</td>
+                    <td>{{ $package?->quantity }}</td>
+                    <td>{{ round($package?->weight, 2) }}</td>
+                    <td>{{ round($package?->volume, 3) }}</td>
+                    <td>{{ $package?->remarks ?? '-' }}</td>
+                </tr>
+            @empty
+                <tr style="text-align: center">
+                    <td colspan="7">No Any Package</td>
+                </tr>
+            @endforelse
+
+            {{-- Add empty rows if it's the first page and less than 50 packages --}}
+            @if ($loop->first && $chunk->count() > 13)
+                @for ($i = 0; $i < 29 - $chunk->count(); $i++)
+                    <tr>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                    </tr>
+                @endfor
+            @endif
+            {{-- Add empty rows if it's the last page and less than 50 packages --}}
+            @if ($loop->last && $chunk->count() > 36)
+                @for ($i = 0; $i < 51 - $chunk->count(); $i++)
+                    <tr>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                    </tr>
+                @endfor
+            @endif
+        </table>
+        @if (!$loop->last || ($loop->last && $chunk->count() > 36))
+            <div class="page-break"></div>
+        @endif
+    @endforeach
+
+
+    <table class="summary">
         <tr>
             <td>NO. OF PCS:</td>
-            <td>{{$hbl?->packages->count()}}</td>
+            <td>{{ $hbl?->packages->count() }}</td>
 
             <td>FREIGHT CHARGES</td>
-            <td>{{number_format($hbl?->freight_charge, 2)}}</td>
+            <td>{{ number_format($hbl?->freight_charge, 2) }}</td>
 
             <td>DESTI. CHARGES</td>
-            <td>{{number_format($hbl?->other_charge, 2)}}</td>
+            <td>{{ number_format($hbl?->other_charge, 2) }}</td>
         </tr>
         <tr>
             <td>TOTAL VOLUME</td>
-            <td>{{round($hbl?->packages->sum('volume'), 3)}}</td>
+            <td>{{ round($hbl?->packages->sum('volume'), 3) }}</td>
 
             <td>HBL CHARGE</td>
-            <td>{{number_format($hbl?->bill_charge, 2)}}</td>
+            <td>{{ number_format($hbl?->bill_charge, 2) }}</td>
 
             <td>GRAND TOTAL</td>
-            <td>{{number_format($hbl?->grand_total, 2)}}</td>
+            <td>{{ number_format($hbl?->grand_total, 2) }}</td>
         </tr>
         <tr>
             <td>TOTAL WEIGHT</td>
-            <td>{{round($hbl?->packages->sum('weight'), 2)}}</td>
+            <td>{{ round($hbl?->packages->sum('weight'), 2) }}</td>
 
             <td>HBL VAT CHARGE</td>
             <td>-</td>
 
             <td>PAID AMOUNT</td>
-            <td>{{number_format($hbl?->paid_amount, 2)}}</td>
+            <td>{{ number_format($hbl?->paid_amount, 2) }}</td>
         </tr>
     </table>
 
     <table class="in-words-table">
         @php
-            $a = new \NumberFormatter("en", \NumberFormatter::SPELLOUT);
+            $a = new \NumberFormatter('en', \NumberFormatter::SPELLOUT);
         @endphp
         <tr>
             <td style="font-weight: bold;">IN WORDS:</td>
-            <td style="text-transform: uppercase">{{$a->format($hbl?->grand_total)}} only</td>
+            <td style="text-transform: uppercase">{{ $a->format($hbl?->grand_total) }} only</td>
         </tr>
     </table>
 @endsection
