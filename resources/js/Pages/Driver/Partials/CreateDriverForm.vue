b749a6cc7cace8419a8cb708833ae4c6a593815b
Ahinsa <isurujayawickramat@gmail.com>
2024-09-19 11:12:35 +0530

    LAC-293: added placeholder

--- resources/js/Pages/Driver/Partials/CreateDriverForm.vue
+++ resources/js/Pages/Driver/Partials/CreateDriverForm.vue
@@ -100,7 +100,7 @@ const createDriver = () => {
                         <input
                             v-model="form.contact"
                             class="form-input peer w-full rounded-lg border border-slate-300 bg-transparent px-3 py-2 pl-9 placeholder:text-slate-400/70 hover:border-slate-400 focus:border-primary dark:border-navy-450 dark:hover:border-navy-400 dark:focus:border-accent"
-                            placeholder="Mobile Number"
+                            placeholder="+94772255888"
                             type="text"
                         />
                         <div
