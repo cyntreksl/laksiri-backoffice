d3612167f48fcdc39f4e613a9468919176ba6c52
Imalsha Weerakkodi <imalsha.weerakkodi@cyntrek.com>
2024-11-05 16:04:41 +0530

    LAC-322: Fix pint issue

--- app/Repositories/ContainerRepositories.php
+++ app/Repositories/ContainerRepositories.php
@@ -171,14 +171,14 @@ class ContainerRepositories implements ContainerRepositoryInterface, GridJsInter
         if (! File::exists($pdfDirectory)) {
             File::makeDirectory($pdfDirectory, 0755, true);
         } else {
-            $pdfFile = new Filesystem();
+            $pdfFile = new Filesystem;
             $pdfFile->cleanDirectory($pdfDirectory);
         }
 
         $container = GetLoadedContainerById::run($container);
 
         // Initialize a new Dompdf instance with custom options
-        $options = new Options();
+        $options = new Options;
         $options->set('isHtml5ParserEnabled', true);
         $dompdf = new Dompdf($options);
 
