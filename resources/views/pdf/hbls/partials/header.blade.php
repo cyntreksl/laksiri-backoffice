14afe860c61b60f4a02652f50cfbd80c7e843f19
Ahinsa <isurujayawickramat@gmail.com>
2024-11-13 13:02:02 +0530

    LAC-332: PDF Updated

--- resources/views/pdf/hbls/partials/header.blade.php
+++ resources/views/pdf/hbls/partials/header.blade.php
@@ -1,7 +1,7 @@
 @if($settings)
     @if($logoPath)
         <div>
-            <img style="max-width: 120px; max-height: 60px; margin-right: -15px; float: left; margin-top: 10px"
+            <img style="max-width: 140px; max-height: 80px; margin-right: -100px; float: left; margin-top: 10px"
                  src="{{ $logoPath }}"
                  alt="app_logo">
         </div>
