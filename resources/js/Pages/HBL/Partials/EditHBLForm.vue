a8c5bcba3d532b36c260c8e5d872e866225faa3b
Ahinsa <isurujayawickramat@gmail.com>
2024-12-03 13:33:07 +0530

    LAC-387: Added Default

--- resources/js/Pages/HBL/Partials/EditHBLForm.vue
+++ resources/js/Pages/HBL/Partials/EditHBLForm.vue
@@ -149,7 +149,9 @@ const packageList = ref(form.packages ?? []);
 
 const packageItem = reactive({
     package_rule: "",
-    type: "",
+    type: props.packageTypes.find(
+        type => type.name.toLowerCase() === 'cartoon'.toLowerCase()
+    )?.name || "",
     length: 0,
     width: 0,
     height: 0,
