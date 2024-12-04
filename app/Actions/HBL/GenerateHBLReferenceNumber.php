0a4ed2a712c52a6fcb51c5a121455083c680b5bb
Imalsha Weerakkodi <imalsha.weerakkodi@cyntrek.com>
2024-12-04 15:20:39 +0530

    LAC-363: hbl ref issue

--- app/Actions/HBL/GenerateHBLReferenceNumber.php
+++ app/Actions/HBL/GenerateHBLReferenceNumber.php
@@ -14,11 +14,11 @@ class GenerateHBLReferenceNumber
         $branch_code = session('current_branch_code');
         $last_hbl = HBL::whereNotNull('reference')->latest()->first();
 
-        if($last_hbl){
+        if ($last_hbl) {
             $extracted = substr($last_hbl->reference, strpos($last_hbl->reference, 'REF') + 3);
         }
 
-        $next_reference = $last_hbl ? ((int)$extracted + 1) : 000001;
+        $next_reference = $last_hbl ? ((int) $extracted + 1) : 000001;
 
         $reference = $branch_code.'-'.'REF'.str_pad($next_reference, 6, '0', STR_PAD_LEFT);
 
