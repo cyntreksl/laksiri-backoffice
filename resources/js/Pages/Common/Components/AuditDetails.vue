a6d08e882f69687ee962c0c8b9400aebf06f43f0
Imalsha Weerakkodi <imalsha.weerakkodi@cyntrek.com>
2024-12-08 11:01:27 +0530

    LAC-348: Fixed Audit Details view

--- resources/js/Pages/Common/Components/AuditDetails.vue
+++ resources/js/Pages/Common/Components/AuditDetails.vue
@@ -1,7 +1,8 @@
 <script setup>
 import SecondaryButton from "@/Components/SecondaryButton.vue";
 import DialogModal from "@/Components/DialogModal.vue";
-import {computed, ref} from "vue";
+import { ref } from "vue";
+import InfoDisplay from "@/Pages/Common/Components/InfoDisplay.vue";
 
 const props = defineProps({
     properties: {
@@ -9,13 +10,7 @@ const props = defineProps({
         default: () => {
         }
     },
-})
-
-const formattedProperties = computed(() => {
-    return Object.entries(props.properties)
-        .map(([key, value]) => `${key}: "${value}"`)
-        .join(", ");
-})
+});
 
 const confirmingShowAuditDetails = ref(false);
 
@@ -26,36 +21,54 @@ const closeModal = () => {
 
 <template>
     <div class="mx-5 mt-4">
-        <svg class="icon icon-tabler icons-tabler-outline icon-tabler-eyeglass text-info cursor-pointer" fill="none"
-             height="24" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24"
-             width="24" xmlns="http://www.w3.org/2000/svg"
-             @click="confirmingShowAuditDetails = !confirmingShowAuditDetails">
-            <path d="M0 0h24v24H0z" fill="none" stroke="none"/>
-            <path d="M8 4h-2l-3 10"/>
-            <path d="M16 4h2l3 10"/>
-            <path d="M10 16l4 0"/>
-            <path d="M21 16.5a3.5 3.5 0 0 1 -7 0v-2.5h7v2.5"/>
-            <path d="M10 16.5a3.5 3.5 0 0 1 -7 0v-2.5h7v2.5"/>
+        <svg
+            class="icon icon-tabler icons-tabler-outline icon-tabler-eyeglass text-info cursor-pointer"
+            fill="none"
+            height="24"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            viewBox="0 0 24 24"
+            width="24"
+            xmlns="http://www.w3.org/2000/svg"
+            @click="confirmingShowAuditDetails = !confirmingShowAuditDetails"
+        >
+            <path d="M0 0h24v24H0z" fill="none" stroke="none" />
+            <path d="M8 4h-2l-3 10" />
+            <path d="M16 4h2l3 10" />
+            <path d="M10 16l4 0" />
+            <path d="M21 16.5a3.5 3.5 0 0 1 -7 0v-2.5h7v2.5" />
+            <path d="M10 16.5a3.5 3.5 0 0 1 -7 0v-2.5h7v2.5" />
         </svg>
     </div>
 
-    <DialogModal :maxWidth="'2xl'" :show="confirmingShowAuditDetails" @close="closeModal">
+    <DialogModal :maxWidth="'5xl'" :show="confirmingShowAuditDetails" @close="closeModal">
         <template #title>
-            Audit Properties
+            <div class="text-lg font-bold">Audit Details</div>
         </template>
 
         <template #content>
-            <div class="break-words whitespace-pre-wrap">
-                <span>
-                    {{ formattedProperties }}
-                </span>
+            <div class="p-2">
+                <dl class="grid grid-cols-3 gap-4">
+                    <div v-for="(value, key) in props.properties" :key="key" class="p-2">
+                        <p class="text-xs uppercase text-slate-400 dark:text-navy-300">
+                            {{ key.replace(/_/g, ' ') }}
+                        </p>
+                        <p class="mt-1 text-xl font-medium text-slate-700 dark:text-navy-100" style="font-size: 1.15rem;">
+                            {{ value || '-' }}
+                        </p>
+                    </div>
+                </dl>
             </div>
         </template>
 
         <template #footer>
-            <SecondaryButton @click="closeModal">
-                Cancel
-            </SecondaryButton>
+            <div class="px-6 py-4">
+                <SecondaryButton @click="closeModal" class="px-6 py-3 rounded-lg bg-gray-200 hover:bg-gray-300 text-gray-700 font-medium">
+                    Close
+                </SecondaryButton>
+            </div>
         </template>
     </DialogModal>
 </template>
