699e34213a5b7f6f682a81796b0a121d786a6dff
Imalsha Weerakkodi <imalsha.weerakkodi@cyntrek.com>
2024-12-10 15:11:44 +0530

    LAC-401: Fixed HBL Number

--- app/Actions/HBL/GenerateHBLNumber.php
+++ app/Actions/HBL/GenerateHBLNumber.php
@@ -14,6 +14,10 @@ class GenerateHBLNumber
         $last_hbl = HBL::where('branch_id', $currentBranchId)->latest()->first();
         $next_number = $last_hbl ? ((int) substr($last_hbl->hbl_number, 3) + 1) : 1;
         $hbl_number = strtoupper(str_pad($currentBranchId, 3, '0', STR_PAD_LEFT)).str_pad($next_number, 6, '0', STR_PAD_LEFT);
+        do {
+            $exists = HBL::where('hbl_number', $hbl_number)->exists();
+            $hbl_number++;
+        } while ($exists);
 
         return $hbl_number;
 
