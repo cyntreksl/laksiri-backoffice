7e89a6996f3d5937fcf2bfc87b9b86597e12d024
Imalsha Weerakkodi <imalsha.weerakkodi@cyntrek.com>
2024-12-14 23:06:01 +0530

    LAC-406: Create migrations

---
+++ app/Models/Officer.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\HasFile;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Officer extends Model
+{
+    use HasFactory;
+    use HasFile;
+    use SoftDeletes;
+
+    protected $table = 'officers';
+
+    protected $fillable = [
+        'type', 'name', 'email', 'mobile_number', 'pp_or_nic_no', 'residency_no', 'address',
+    ];
+}
