ce2b88d4124c0ae299271666a3cead839eb8bcd2
Imalsha Weerakkodi <imalsha.weerakkodi@cyntrek.com>
2024-12-17 18:00:49 +0530

    LAC-410: Remove MHBL from Lodding point

--- app/Models/Officer.php
+++ app/Models/Officer.php
@@ -2,13 +2,16 @@
 
 namespace App\Models;
 
+use App\Models\Scopes\BranchScope;
 use App\Traits\HasFile;
+use Illuminate\Database\Eloquent\Attributes\ScopedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\Activitylog\LogOptions;
 use Spatie\Activitylog\Traits\LogsActivity;
 
+#[ScopedBy(BranchScope::class)]
 class Officer extends Model
 {
     use HasFactory;
