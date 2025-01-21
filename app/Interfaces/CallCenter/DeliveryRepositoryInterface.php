e0bf55393cfb94c216b17fba2f50f88a9de55f1c
Imalsha Weerakkodi <imalsha.weerakkodi@cyntrek.com>
2025-01-21 15:48:21 +0530

    LAC-516: Completed API

--- app/Interfaces/CallCenter/DeliveryRepositoryInterface.php
+++ app/Interfaces/CallCenter/DeliveryRepositoryInterface.php
@@ -2,9 +2,14 @@
 
 namespace App\Interfaces\CallCenter;
 
+use App\Models\HBLDeliver;
+use Illuminate\Http\JsonResponse;
+
 interface DeliveryRepositoryInterface
 {
     public function assignDriverToDeliver(array $data);
 
     public function getPendingDeliverForDriver();
+
+    public function showDeliver(HBLDeliver $hblDeliver): JsonResponse;
 }
