49b4a4df26d30468d1ecdd9ea730bda6f9d28cf8
Chinthaka Dilan F <ccdilan@gmail.com>
2025-02-04 16:51:24 +0530

    LAC-541: PINT

--- app/Http/Controllers/WhatsappController.php
+++ app/Http/Controllers/WhatsappController.php
@@ -7,10 +7,11 @@ use Illuminate\Support\Facades\Log;
 
 class WhatsappController extends Controller
 {
-   public function webhook(Request $request)
-   {
-       $data = $request->all();
-       Log::info('Whatsapp Webhook', $data);
-       return response()->json(['status' => 'success']);
-   }
+    public function webhook(Request $request)
+    {
+        $data = $request->all();
+        Log::info('Whatsapp Webhook', $data);
+
+        return response()->json(['status' => 'success']);
+    }
 }
