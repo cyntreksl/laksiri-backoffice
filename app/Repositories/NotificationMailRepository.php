e53282fcd674513f2897ea0b5b015dd25eda2644
Imalsha Weerakkodi <imalsha.weerakkodi@cyntrek.com>
2025-02-05 16:43:50 +0530

    LAC-521: Updated

--- app/Repositories/NotificationMailRepository.php
+++ app/Repositories/NotificationMailRepository.php
@@ -18,6 +18,22 @@ class NotificationMailRepository implements NotificationMailRepositoryInterface
         $this->settings = GetSettings::run();
     }
 
+    public function sendPickupCreationNotification(PickUp $pickUp)
+    {
+        $notification_settings = ! empty($this->settings->notification)
+            ? json_decode($this->settings->notification, true)
+            : null;
+        if ($notification_settings && isset($notification_settings['Email']) && $notification_settings['Email'] === true) {
+            $email_data = [
+                'subject' => 'Booking Confirmation',
+                'customer_name' => $pickUp->name,
+                'success_message' => 'Thank you for booking with us. Your booking is confirmed. ',
+                'detail_message' => 'Booking Reference Number: '.$pickUp->reference.' We will notify you with updates.',
+            ];
+            Mail::to($pickUp->email)->send(new Notification($email_data));
+        }
+    }
+
     public function sendAssignDriverNotification(PickUp $pickUp)
     {
         $notification_settings = json_decode($this->settings->notification, true);
