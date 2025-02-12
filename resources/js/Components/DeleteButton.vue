96032062107fb7319508ec1849da10461770fe4e
Tharusha <74040427+Tharusha01@users.noreply.github.com>
2025-02-12 14:38:12 +0530

    LAC-570:wip

---
+++ resources/js/Components/DeleteButton.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+    type: {
+        type: String,
+        default: 'submit',
+    },
+});
+</script>
+
+<template>
+    <button :type="type"
+            class="inline-flex items-center btn bg-[#EC6239] font-medium text-white hover:bg-[#D65532] focus:bg-[#D65532] active:bg-[#C14B2C]/90 disabled:pointer-events-none disabled:select-none disabled:opacity-60">
+        <slot />
+    </button>
+</template>
+<!-- Poya dina Ciyntrek wetha bathi bara sithin sadana ladi  -->
--- resources/js/Pages/Pickup/PendingJobs.vue
+++ resources/js/Pages/Pickup/PendingJobs.vue
@@ -21,6 +21,7 @@ import DeletePickupConfirmationModal from "@/Pages/Pickup/Partials/DeletePickupC
 import SimpleOverviewWidget from "@/Components/Widgets/SimpleOverviewWidget.vue";
 import HBLDetailModal from "@/Pages/Common/HBLDetailModal.vue";
 import RetryPickupConfirmationModal from "@/Pages/Pickup/Partials/RetryPickupConfirmationModal.vue";
+import DeleteButton from "@/Components/DeleteButton.vue";
 
 const props = defineProps({
     drivers: {
@@ -1142,6 +1143,31 @@ const shipIcon = ref(`
                                 </svg>
                                 Assign Driver ({{ countOfSelectedData }})
                             </PrimaryButton>
+                        </div>
+
+                        <div class="flex justify-end flex-row-reverse">
+                            <br>
+                            <DeleteButton
+                                v-if="$page.props.user.permissions.includes('pickups.delete')"
+                                :disabled="isDataEmpty"
+                                class="flex"
+                                @click="confirmDeletePickup"
+                            >
+                                <svg  xmlns="http://www.w3.org/2000/svg"
+                                      width="24"
+                                      height="24"
+                                      viewBox="0 0 24 24"
+                                      fill="none"
+                                      stroke="currentColor"
+                                      stroke-width="2"
+                                      stroke-linecap="round"
+                                      stroke-linejoin="round"
+                                      class="icon icon-tabler icons-tabler-outline icon-tabler-trash">
+                                    <path
+                                        stroke="none" d="M0 0h24v24H0z"
+                                        fill="none"/><path d="M4 7l16 0" /><path d="M10 11l0 6" /><path d="M14 11l0 6" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></svg>
+                                Delete ({{ countOfSelectedData }})
+                            </DeleteButton>
                         </div>
                     </div>
                 </div>
