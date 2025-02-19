63fc5537434f80ca86f4ec9e7d0ac53b5bc8d169
Imalsha Weerakkodi <imalsha.weerakkodi@cyntrek.com>
2025-02-19 17:12:08 +0530

    LAC-338: Updated

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -251,18 +251,7 @@
                 openSideBar();
               "
                             >
-                                <svg class="icon icon-tabler icons-tabler-outline icon-tabler-screen-share" fill="none" height="24" stroke="currentColor"
-                                     stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24"
-                                     width="24"
-                                     xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M0 0h24v24H0z" fill="none" stroke="none"/>
-                                    <path d="M21 12v3a1 1 0 0 1 -1 1h-16a1 1 0 0 1 -1 -1v-10a1 1 0 0 1 1 -1h9"/>
-                                    <path d="M7 20l10 0"/>
-                                    <path d="M9 16l0 4"/>
-                                    <path d="M15 16l0 4"/>
-                                    <path d="M17 4h4v4"/>
-                                    <path d="M16 9l5 -5"/>
-                                </svg>
+                                <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-package"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 3l8 4.5l0 9l-8 4.5l-8 -4.5l0 -9l8 -4.5" /><path d="M12 12l8 -4.5" /><path d="M12 12l0 9" /><path d="M12 12l-8 -4.5" /><path d="M16 5.25l-8 4.5" /></svg>
                             </a>
 
                             <!-- Examination  -->
--- resources/js/Pages/CallCenter/HBL/HBLList.vue
+++ resources/js/Pages/CallCenter/HBL/HBLList.vue
@@ -393,34 +393,26 @@ const createColumns = () => [
                                 "'Download Baggage PDF'",
                         },
                         [
-                            h(
-                                "svg",
+                            h('svg',
                                 {
-                                    xmlns: "http://www.w3.org/2000/svg",
-                                    viewBox: "0 0 24 24",
-                                    class: "icon icon-tabler icons-tabler-outline icon-tabler-download",
-                                    fill: "none",
-                                    height: 24,
-                                    width: 24,
-                                    stroke: "currentColor",
-                                    strokeLinecap: "round",
-                                    strokeLinejoin: "round",
-                                },
-                                [
-                                    h("path", {
-                                        d: "M0 0h24v24H0z",
-                                        fill: "none",
-                                        stroke: "none",
-                                    }),
-                                    h("path", {
-                                        d: "M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2",
-                                    }),
-                                    h("path", {
-                                        d: "M7 11l5 5l5 -5",
-                                    }),
-                                    h("path", {
-                                        d: "M12 4l0 12",
-                                    }),
+                                    xmlns: 'http://www.w3.org/2000/svg',
+                                    width: '24',
+                                    height: '24',
+                                    viewBox: '0 0 24 24',
+                                    fill: 'none',
+                                    stroke: 'currentColor',
+                                    'stroke-width': '2',
+                                    'stroke-linecap': 'round',
+                                    'stroke-linejoin': 'round',
+                                    class: 'icon icon-tabler icons-tabler-outline icon-tabler-file-type-pdf'
+                                }, [
+                                    h('path', { stroke: 'none', d: 'M0 0h24v24H0z', fill: 'none' }),
+                                    h('path', { d: 'M14 3v4a1 1 0 0 0 1 1h4' }),
+                                    h('path', { d: 'M5 12v-7a2 2 0 0 1 2 -2h7l5 5v4' }),
+                                    h('path', { d: 'M5 18h1.5a1.5 1.5 0 0 0 0 -3h-1.5v6' }),
+                                    h('path', { d: 'M17 18h2' }),
+                                    h('path', { d: 'M20 15h-3v6' }),
+                                    h('path', { d: 'M11 15v6h1a2 2 0 0 0 2 -2v-2a2 2 0 0 0 -2 -2h-1z' })
                                 ]
                             ),
                         ]
