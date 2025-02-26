54895365cb3ccd4085af017ed5724f1f4e3e086f
Imalsha Weerakkodi <imalsha.weerakkodi@cyntrek.com>
2025-02-26 12:49:44 +0530

    LAC-341: Warehouse must be Destinations branches

--- resources/js/Pages/HBL/HBLList.vue
+++ resources/js/Pages/HBL/HBLList.vue
@@ -38,6 +38,11 @@ const props = defineProps({
         default: () => {
         },
     },
+    warehouses: {
+        type: Object,
+        default: () => {
+        },
+    },
 });
 
 const wrapperRef = ref(null);
@@ -54,7 +59,7 @@ const filters = reactive({
     cargoMode: ["Air Cargo", "Sea Cargo"],
     hblType: ["UPB", "Gift", "Door to Door"],
     isHold: false,
-    warehouse: ["COLOMBO", "NINTAVUR", "OTHER"],
+    warehouse: props.warehouses.map(warehouse => warehouse.name),
     createdBy: "",
     paymentStatus: [],
 });
@@ -636,7 +641,7 @@ const resetFilter = () => {
     filters.cargoMode = ["Air Cargo", "Sea Cargo", "Door to Door"];
     filters.hblType = ["UPB", "Gift", "Door to Door"];
     filters.isHold = false;
-    filters.warehouse = ["COLOMBO", "NINTAVUR", "OTHER"];
+    filters.warehouse = props.warehouses.map(warehouse => warehouse.name);
     filters.createdBy = "";
     filters.paymentStatus = [];
     applyFilters();
@@ -1174,27 +1179,15 @@ const shipIcon = ref(`
 
                 <FilterHeader value="Warehouse"/>
 
-                <label class="inline-flex items-center space-x-2 mt-2">
+                <label
+                    v-for="warehouse in warehouses"
+                    :key="warehouse.id"
+                    class="inline-flex items-center space-x-2 mt-2"
+                >
                     <Switch
                         v-model="filters.warehouse"
-                        label="COLOMBO"
-                        value="COLOMBO"
-                    />
-                </label>
-
-                <label class="inline-flex items-center space-x-2 mt-2">
-                    <Switch
-                        v-model="filters.warehouse"
-                        label="NINTAVUR"
-                        value="NINTAVUR"
-                    />
-                </label>
-
-                <label class="inline-flex items-center space-x-2 mt-2">
-                    <Switch
-                        v-model="filters.warehouse"
-                        label="OTHER"
-                        value="OTHER"
+                        :label="warehouse.name"
+                        :value="warehouse.name"
                     />
                 </label>
 
