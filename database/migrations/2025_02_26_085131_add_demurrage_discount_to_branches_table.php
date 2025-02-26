0ca9cf24204972b70ce6d5cd2f329feddb07c991
Imalsha Weerakkodi <imalsha.weerakkodi@cyntrek.com>
2025-02-26 14:49:14 +0530

    LAC-595: Demurrage Discount

---
+++ database/migrations/2025_02_26_085131_add_demurrage_discount_to_branches_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('branches', function (Blueprint $table) {
+            $table->integer('demurrage_discount')->default(0)->after('container_delays');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('branches', function (Blueprint $table) {
+            $table->dropColumn('demurrage_discount');
+        });
+    }
+};
