9f0060251feea540fc2646c7e47a3d16174ad014
Chinthaka Dilan F <ccdilan@gmail.com>
2025-03-08 11:55:26 +0530

    LACK-636: Remove try catch

--- app/Actions/PickUps/CreatePickUp.php
+++ app/Actions/PickUps/CreatePickUp.php
@@ -27,36 +27,36 @@ class CreatePickUp
             $package_types = isset($data['package_types']) ? json_encode($data['package_types']) : json_encode($data['note_type']);
         }
 
-        try {
-            $pickup = PickUp::create([
-                'reference' => GeneratePickupReferenceNumber::run(GetUserCurrentBranch::run()['branchName']),
-                'branch_id' => GetUserCurrentBranch::run()['branchId'],
-                'cargo_type' => $data['cargo_type'],
-                'name' => Str::title($data['name']),
-                'email' => $data['email'],
-                'contact_number' => $data['contact_number'],
-                'additional_mobile_number' => $data['additional_mobile_number'],
-                'whatsapp_number' => ! empty($data['whatsapp_number']) ? $data['whatsapp_number'] : null,
-                'address' => Str::title($data['address']),
-                'location_name' => $data['location'] ?? null,
-                'zone_id' => $data['zone_id'] ?? null,
-                'notes' => ! empty($data['notes']) ? Str::title($data['notes']) : null,
-                'package_types' => $package_types,
-                'pickup_date' => ! empty($data['pickup_date']) ? $data['pickup_date'] : null,
-                'pickup_time_start' => ! empty($data['pickup_time_start']) ? $data['pickup_time_start'] : null,
-                'pickup_time_end' => ! empty($data['pickup_time_end']) ? $data['pickup_time_end'] : null,
-                'pickup_type' => $data['pickup_type'] ?? null,
-                'pickup_note' => $pickup_note,
-                'created_by' => auth()->id(),
-                'system_status' => PickUp::SYSTEM_STATUS_PICKUP_CREATED,
-            ]);
+        //        try {
+        $pickup = PickUp::create([
+            'reference' => GeneratePickupReferenceNumber::run(GetUserCurrentBranch::run()['branchName']),
+            'branch_id' => GetUserCurrentBranch::run()['branchId'],
+            'cargo_type' => $data['cargo_type'],
+            'name' => Str::title($data['name']),
+            'email' => $data['email'],
+            'contact_number' => $data['contact_number'],
+            'additional_mobile_number' => $data['additional_mobile_number'],
+            'whatsapp_number' => ! empty($data['whatsapp_number']) ? $data['whatsapp_number'] : null,
+            'address' => Str::title($data['address']),
+            'location_name' => $data['location'] ?? null,
+            'zone_id' => $data['zone_id'] ?? null,
+            'notes' => ! empty($data['notes']) ? Str::title($data['notes']) : null,
+            'package_types' => $package_types,
+            'pickup_date' => ! empty($data['pickup_date']) ? $data['pickup_date'] : null,
+            'pickup_time_start' => ! empty($data['pickup_time_start']) ? $data['pickup_time_start'] : null,
+            'pickup_time_end' => ! empty($data['pickup_time_end']) ? $data['pickup_time_end'] : null,
+            'pickup_type' => $data['pickup_type'] ?? null,
+            'pickup_note' => $pickup_note,
+            'created_by' => auth()->id(),
+            'system_status' => PickUp::SYSTEM_STATUS_PICKUP_CREATED,
+        ]);
 
-            $pickup->addStatus('Pickup Created');
+        $pickup->addStatus('Pickup Created');
 
-            return $pickup;
-        } catch (\Exception $e) {
-            Log::error($e->getMessage());
-            throw new \Exception('Failed to create pickup');
-        }
+        return $pickup;
+        //        } catch (\Exception $e) {
+        //            Log::error($e->getMessage());
+        //            throw new \Exception('Failed to create pickup');
+        //        }
     }
 }
