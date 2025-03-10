86cc194b5544b83a3e1eb479886cc73aa9b87451
Ahinsa <isurujayawickramat@gmail.com>
2025-03-10 13:33:56 +0530

    LAC-640: warehouse table > filter ( Colombo , ninthavur ) ( upb, gift , door to door )

---
+++ app/Factory/Warehouse/Filters/WarehouseFilter.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Factory\Warehouse\Filters;
+
+use App\Interfaces\FilterInterface;
+use Illuminate\Database\Eloquent\Builder;
+
+class WarehouseFilter implements FilterInterface
+{
+    public function apply(Builder $query, $value)
+    {
+        if (! is_null($value)) {
+            $value = ! is_array($value) ? explode(',', $value) : $value;
+
+            return $query->whereIn('warehouse', $value);
+        }
+
+        return $query;
+    }
+}
