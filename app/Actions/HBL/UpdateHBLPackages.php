8f88e0433ad1dddfaa8b27c3e617c7da06f6a74a
Imalsha Weerakkodi <imalsha.weerakkodi@cyntrek.com>
2025-03-24 14:10:57 +0530

    LAC-642-V1: Updated

--- app/Actions/HBL/UpdateHBLPackages.php
+++ app/Actions/HBL/UpdateHBLPackages.php
@@ -31,18 +31,6 @@ class UpdateHBLPackages
             });
             foreach ($deletedPackages as $packageToDelete) {
                 $packageToDelete->delete();
-            $existPackages = array_filter($data, function ($item) {
-                return isset($item['id']);
-            });
-            $newPackages = array_filter($data, function ($item) {
-                return ! isset($item['id']);
-            });
-            $existPackageIds = collect($existPackages)->pluck('id')->toArray();
-            $deletedPackages = $hbl->packages->reject(function ($package) use ($existPackageIds) {
-                return in_array($package->id, $existPackageIds);
-            });
-            foreach ($deletedPackages as $packageToDelete) {
-                $packageToDelete->delete();
             }
             CreateHBLPackages::run($hbl, $newPackages);
         });
