331407a08065b589fda92b8de3d38234c42d6654
Imalsha Weerakkodi <imalsha.weerakkodi@cyntrek.com>
2025-04-02 11:02:05 +0530

    LAC-629: Updated

---
+++ routes/finance-breadcrumbs.php
@@ -0,0 +1,16 @@
+<?php
+
+use Diglactic\Breadcrumbs\Breadcrumbs;
+use Diglactic\Breadcrumbs\Generator as BreadcrumbTrail;
+
+// HBLs
+Breadcrumbs::for('finance.hbls.index', function (BreadcrumbTrail $trail) {
+    $trail->push('HBL');
+    $trail->push('HBL List', route('finance.hbls.index'));
+});
+
+// Approve HBLs
+Breadcrumbs::for('finance.hbls.approve-hbl', function (BreadcrumbTrail $trail) {
+    $trail->push('HBL');
+    $trail->push('Approve HBLs', route('finance.hbls.approve-hbl'));
+});
