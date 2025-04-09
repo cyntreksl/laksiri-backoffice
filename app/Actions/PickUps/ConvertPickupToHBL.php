701e7a328a2e372adab81dff01f36b799a1682e5
Imalsha Weerakkodi <imalsha.weerakkodi@cyntrek.com>
2025-04-09 13:53:04 +0530

    LAC-691: debug

--- app/Actions/PickUps/ConvertPickupToHBL.php
+++ app/Actions/PickUps/ConvertPickupToHBL.php
@@ -23,8 +23,6 @@ class ConvertPickupToHBL
             ? 'Door to Door'
             : ($request->hbl_type ?? $pickup->hbl_type);
 
-        dd($request->all());
-
         $warehouse = GetBranchByName::run($request->warehouse);
         $data = [
             'cargo_type' => $request->cargo_type ?: $pickup->cargo_type,
