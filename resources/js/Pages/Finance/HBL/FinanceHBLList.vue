634e48dcff02619beccd7199d651ec51c7775867
Imalsha Weerakkodi <imalsha.weerakkodi@cyntrek.com>
2025-04-28 12:02:31 +0530

    LAC-629: Updated

--- resources/js/Pages/Finance/HBL/FinanceHBLList.vue
+++ resources/js/Pages/Finance/HBL/FinanceHBLList.vue
@@ -401,36 +401,10 @@ const resolvePaymentStatus = (paymentStatus) => {
     }
 };
 
-// const approveHBLs = async () => {
-//     const idList = selectedHBLs.value.map((item) => item.id);
-//
-//     try {
-//         const response = await fetch("/finance/finance-approved", {
-//             method: "POST",
-//             headers: {
-//                 "Content-Type": "application/json",
-//                 "X-CSRF-TOKEN": document
-//                     .querySelector('meta[name="csrf-token"]')
-//                     .getAttribute("content"),
-//             },
-//             body: JSON.stringify({hbl_ids: idList}),
-//         });
-//
-//         if (!response.ok) {
-//             throw new Error("Network response was not ok.");
-//         } else {
-//             window.location.reload();
-//             push.success("HBLs Approved Successfully!");
-//         }
-//     } catch (error) {
-//         console.error("Error:", error);
-//     }
-// };
-
 const approveHBLs = () => {
     const idList = selectedHBLs.value.map((item) => item.id);
     confirm.require({
-        message: 'Would you like to approve this hbl records?',
+        message: 'Would you like to approve these HBL records?',
         header: 'Approve HBLs?',
         icon: 'pi pi-info-circle',
         rejectLabel: 'Cancel',
@@ -444,27 +418,32 @@ const approveHBLs = () => {
             severity: 'success'
         },
         accept: async () => {
-            const response = await fetch("/finance/finance-approved", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    "X-CSRF-TOKEN": document
-                        .querySelector('meta[name="csrf-token"]')
-                        .getAttribute("content"),
-                },
-                body: JSON.stringify({hbl_ids: idList}),
-            });
+            try {
+                const response = await fetch("/finance/finance-approved", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "X-CSRF-TOKEN": document
+                            .querySelector('meta[name="csrf-token"]')
+                            .getAttribute("content"),
+                    },
+                    body: JSON.stringify({ hbl_ids: idList }),
+                });
 
-        if (!response.ok) {
-            throw new Error("Network response was not ok.");
-        } else {
-            window.location.reload();
-            push.success("HBLs Approved Successfully!");
+                if (!response.ok) {
+                    throw new Error("Network response was not ok.");
+                } else {
+                    push.success("HBLs Approved Successfully!");
+                    window.location.reload();
+                }
+            } catch (error) {
+                console.error("Error:", error);
+                push.error("Failed to approve HBLs.");
+            }
         }
-    } catch (error) {
-        console.error("Error:", error);
-    }
+    });
 };
+
 
 </script>
 
--- routes/web/finance/hbl.php
+++ routes/web/finance/hbl.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::resource('hbls', App\Http\Controllers\HBLController::class)->except(['store', 'update', 'destroy', 'create', 'edit']);
-
-Route::get('hbl-list', [App\Http\Controllers\HBLController::class, 'list']);
-
 Route::get('approve-hbls', [App\Http\Controllers\Finance\HBLController::class, 'approveHBLs'])->name('hbls.approve-hbl');
 
 Route::get('approve-hbl-list', [App\Http\Controllers\Finance\HBLController::class, 'list']);
