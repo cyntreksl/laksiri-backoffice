1f09d509e6f426c2df92dcaff594c24a2ec0cda1
Ahinsa <isurujayawickramat@gmail.com>
2025-04-29 15:45:39 +0530

    LAC-745: Fixed Cash Settlement Payment Filter Issue

--- resources/js/Pages/CashSettlement/Partials/PaymentModal.vue
+++ resources/js/Pages/CashSettlement/Partials/PaymentModal.vue
@@ -82,7 +82,7 @@ const handleUpdatePayment = () => {
             <InputNumber v-show="(hbl?.grand_total - hbl?.paid_amount) !== 0"
                          v-model="form.paid_amount"
                          :max="hbl?.grand_total - hbl?.paid_amount"
-                         :maxFractionDigits="5"
+                         :maxFractionDigits="2"
                          :minFractionDigits="2"
                          class="w-full"
                          fluid
