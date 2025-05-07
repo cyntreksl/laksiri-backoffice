babb1b1fd2ccca2ca238987ac3e8c23ad2e61641
Ahinsa <isurujayawickramat@gmail.com>
2025-05-07 13:39:31 +0530

    LAC-765: Examination Queue UI Updated

--- resources/js/Pages/CallCenter/Examination/QueueList.vue
+++ resources/js/Pages/CallCenter/Examination/QueueList.vue
@@ -1,22 +1,33 @@
 <script setup>
-import {Link} from "@inertiajs/vue3";
-import DestinationAppLayout from "@/Layouts/DestinationAppLayout.vue";
 import Breadcrumb from "@/Components/Breadcrumb.vue";
-import { computed } from "vue";
+import {computed, ref} from "vue";
 import DashboardCard from "@/Components/Widgets/DashboardCard.vue";
 import AppLayout from "@/Layouts/AppLayout.vue";
+import {router} from "@inertiajs/vue3";
+import Column from "primevue/column";
+import Card from "primevue/card";
+import DataTable from "primevue/datatable";
+import DataView from "primevue/dataview";
+import Button from "primevue/button";
+import SelectButton from "primevue/selectbutton";
+import Tag from "primevue/tag";
 
 const props = defineProps({
     examinationQueue: {
         type: Object,
-        default: () => {}
+        default: () => {
+        }
     },
     examinationQueueCounts: {
         type: Object,
-        default: () => {}
+        default: () => {
+        }
     }
 })
-console.log(props.examinationQueue);
+
+const layout = ref('list');
+const options = ref(['list', 'grid']);
+
 const filteredExaminationQueue = computed(() => {
     return props.examinationQueue.filter(q => {
         return q.is_verified === true && q.is_paid === true && q.is_force_released === false
@@ -25,70 +36,133 @@ const filteredExaminationQueue = computed(() => {
 </script>
 
 <template>
-    <AppLayout title="Queue List">
-        <template #header>Queue List</template>
+    <AppLayout title="Examination Queue">
+        <template #header>Examination Queue</template>
 
-        <Breadcrumb />
+        <Breadcrumb/>
 
         <div class="grid grid-cols-1 gap-3 mt-4 sm:grid-cols-2 md:grid-cols-3">
-            <DashboardCard :count="props.examinationQueueCounts.total" icon="briefcase" icon-color="secondary" title="Total"/>
-            <DashboardCard :count="props.examinationQueueCounts.pending" icon="hourglass-half" icon-color="warning" title="Pending Job"/>
-            <DashboardCard :count="props.examinationQueueCounts.completed" icon="thumbs-up" icon-color="success" title="Completed"/>
+            <DashboardCard :count="props.examinationQueueCounts.total" icon="briefcase" icon-color="secondary"
+                           title="Total"/>
+            <DashboardCard :count="props.examinationQueueCounts.pending" icon="hourglass-half" icon-color="warning"
+                           title="Pending Job"/>
+            <DashboardCard :count="props.examinationQueueCounts.completed" icon="thumbs-up" icon-color="success"
+                           title="Completed"/>
         </div>
 
 
-        <div v-if="Object.keys(filteredExaminationQueue).length > 0" class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 my-5">
-            <template v-for="queue in filteredExaminationQueue" :key="queue.id">
-                <Link v-if="queue.is_released_from_boned_area" :href="route('call-center.examination.create', queue.id)" class="card grow cursor-pointer hover:bg-green-200 items-center p-4 text-center sm:p-5 border w-60 rounded-lg border-green-500">
-                    <div class="my-5">
-                        <h1 class="text-7xl text-black font-bold">{{ queue.token }}</h1>
+        <DataView :layout="layout" :value="filteredExaminationQueue" class="my-5">
+            <template #header>
+                <div class="flex justify-between items-center">
+                    <div class="text-lg font-medium">
+                        Examination Queue
                     </div>
-                    <div class="my-2 grow">
-                        <h3 class="text-lg font-medium text-slate-700 dark:text-navy-100">
-                            {{ queue.hbl?.hbl_number }}
-                        </h3>
-                    </div>
-                    <div class="mt-3 space-y-1">
-                        <button
-                            class="btn h-7 rounded-full bg-slate-150 px-3 text-xs+ font-medium text-slate-800 hover:bg-slate-200 focus:bg-slate-200 active:bg-slate-200/80 dark:bg-navy-500 dark:text-navy-50 dark:hover:bg-navy-450 dark:focus:bg-navy-450 dark:active:bg-navy-450/90">
-                            {{ queue.customer }}
-                        </button>
-
-                        <div v-if="queue.is_released_from_boned_area" class="flex text-xs items-center">
-                            <svg  class="icon icon-tabler icons-tabler-filled icon-tabler-rosette-discount-check text-success mr-2"  fill="currentColor"  height="24"  viewBox="0 0 24 24"  width="24"  xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none" stroke="none"/><path d="M12.01 2.011a3.2 3.2 0 0 1 2.113 .797l.154 .145l.698 .698a1.2 1.2 0 0 0 .71 .341l.135 .008h1a3.2 3.2 0 0 1 3.195 3.018l.005 .182v1c0 .27 .092 .533 .258 .743l.09 .1l.697 .698a3.2 3.2 0 0 1 .147 4.382l-.145 .154l-.698 .698a1.2 1.2 0 0 0 -.341 .71l-.008 .135v1a3.2 3.2 0 0 1 -3.018 3.195l-.182 .005h-1a1.2 1.2 0 0 0 -.743 .258l-.1 .09l-.698 .697a3.2 3.2 0 0 1 -4.382 .147l-.154 -.145l-.698 -.698a1.2 1.2 0 0 0 -.71 -.341l-.135 -.008h-1a3.2 3.2 0 0 1 -3.195 -3.018l-.005 -.182v-1a1.2 1.2 0 0 0 -.258 -.743l-.09 -.1l-.697 -.698a3.2 3.2 0 0 1 -.147 -4.382l.145 -.154l.698 -.698a1.2 1.2 0 0 0 .341 -.71l.008 -.135v-1l.005 -.182a3.2 3.2 0 0 1 3.013 -3.013l.182 -.005h1a1.2 1.2 0 0 0 .743 -.258l.1 -.09l.698 -.697a3.2 3.2 0 0 1 2.269 -.944zm3.697 7.282a1 1 0 0 0 -1.414 0l-3.293 3.292l-1.293 -1.292l-.094 -.083a1 1 0 0 0 -1.32 1.497l2 2l.094 .083a1 1 0 0 0 1.32 -.083l4 -4l.083 -.094a1 1 0 0 0 -.083 -1.32z" /></svg>
-
-                            Released From Boned Area
-                        </div>
-                    </div>
-                </Link>
-
-                <div v-else class="card grow cursor-not-allowed items-center p-4 text-center sm:p-5 border w-60 rounded-lg border-red-500">
-                    <div class="my-5">
-                        <h1 class="text-7xl text-black font-bold">{{ queue.token }}</h1>
-                    </div>
-                    <div class="my-2 grow">
-                        <h3 class="text-lg font-medium text-slate-700 dark:text-navy-100">
-                            {{ queue.hbl?.hbl_number }}
-                        </h3>
-                    </div>
-                    <div class="mt-3 space-y-1">
-                        <button
-                            class="btn h-7 rounded-full bg-slate-150 px-3 text-xs+ font-medium text-slate-800 hover:bg-slate-200 focus:bg-slate-200 active:bg-slate-200/80 dark:bg-navy-500 dark:text-navy-50 dark:hover:bg-navy-450 dark:focus:bg-navy-450 dark:active:bg-navy-450/90">
-                            {{ queue.customer }}
-                        </button>
-
-                        <div v-if="queue.is_released_from_boned_area" class="flex text-xs items-center">
-                            <svg  class="icon icon-tabler icons-tabler-filled icon-tabler-rosette-discount-check text-success mr-2"  fill="currentColor"  height="24"  viewBox="0 0 24 24"  width="24"  xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none" stroke="none"/><path d="M12.01 2.011a3.2 3.2 0 0 1 2.113 .797l.154 .145l.698 .698a1.2 1.2 0 0 0 .71 .341l.135 .008h1a3.2 3.2 0 0 1 3.195 3.018l.005 .182v1c0 .27 .092 .533 .258 .743l.09 .1l.697 .698a3.2 3.2 0 0 1 .147 4.382l-.145 .154l-.698 .698a1.2 1.2 0 0 0 -.341 .71l-.008 .135v1a3.2 3.2 0 0 1 -3.018 3.195l-.182 .005h-1a1.2 1.2 0 0 0 -.743 .258l-.1 .09l-.698 .697a3.2 3.2 0 0 1 -4.382 .147l-.154 -.145l-.698 -.698a1.2 1.2 0 0 0 -.71 -.341l-.135 -.008h-1a3.2 3.2 0 0 1 -3.195 -3.018l-.005 -.182v-1a1.2 1.2 0 0 0 -.258 -.743l-.09 -.1l-.697 -.698a3.2 3.2 0 0 1 -.147 -4.382l.145 -.154l.698 -.698a1.2 1.2 0 0 0 .341 -.71l.008 -.135v-1l.005 -.182a3.2 3.2 0 0 1 3.013 -3.013l.182 -.005h1a1.2 1.2 0 0 0 .743 -.258l.1 -.09l.698 -.697a3.2 3.2 0 0 1 2.269 -.944zm3.697 7.282a1 1 0 0 0 -1.414 0l-3.293 3.292l-1.293 -1.292l-.094 -.083a1 1 0 0 0 -1.32 1.497l2 2l.094 .083a1 1 0 0 0 1.32 -.083l4 -4l.083 -.094a1 1 0 0 0 -.083 -1.32z" /></svg>
-
-                            Released From Boned Area
-                        </div>
-                    </div>
+                    <SelectButton v-model="layout" :allowEmpty="false" :options="options">
+                        <template #option="{ option }">
+                            <i :class="[option === 'list' ? 'pi pi-bars' : 'pi pi-table']"/>
+                        </template>
+                    </SelectButton>
                 </div>
             </template>
-        </div>
 
-        <div v-else class="flex justify-center mt-20 w-full">
-            <p class="text-xl">No Tokens Available</p>
-        </div>
+            <template #list="slotProps">
+                <DataTable :rows="10" :rowsPerPageOptions="[5, 10, 20, 50]" :value="slotProps.items" paginator row-hover
+                           tableStyle="min-width: 50rem">
+                    <Column field="token" header="Token">
+                        <template #body="slotProps">
+                            <div class="flex items-center text-2xl">
+                                <i v-if="slotProps.data.is_released_from_boned_area"
+                                   v-tooltip="'Released From Boned Area'"
+                                   class="ti ti-tag-starred mr-1 text-success"></i>
+                                <i v-else v-tooltip="'Not Released From Boned Area'"
+                                   class="ti ti-tag-off mr-1 text-error"></i>
+                                {{ slotProps.data.token }}
+                            </div>
+                        </template>
+                    </Column>
+                    <Column field="customer" header="Customer"></Column>
+                    <Column field="reference" header="Reference"></Column>
+                    <Column field="package_count" header="Packages">
+                        <template #body="slotProps">
+                            <div class="flex items-center">
+                                <i class="ti ti-package mr-1 text-blue-500" style="font-size: 1rem"></i>
+                                {{ slotProps.data.package_count }}
+                            </div>
+                        </template>
+                    </Column>
+                    <Column field="created_at" header="Created At"></Column>
+                    <Column field="" style="width: 10%">
+                        <template #body="{ data }">
+                            <Button
+                                v-if="data.is_released_from_boned_area"
+                                class="mr-2"
+                                icon="ti ti-arrow-right"
+                                rounded
+                                size="small"
+                                @click.prevent="() => router.visit(route('call-center.examination.create', data.id))"
+                            />
+                        </template>
+                    </Column>
+                    <template #footer> In total there are {{ slotProps.items.length }} tokens.</template>
+                </DataTable>
+            </template>
+
+            <template #grid="slotProps">
+                <div
+                    class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 my-5 p-5">
+                    <Card
+                        v-for="queue in slotProps.items"
+                        :key="queue.id"
+                        :class="[
+    'rounded-2xl bg-white',
+    queue.is_released_from_boned_area ? 'border border-success hover:bg-success/10 cursor-pointer' : 'border border-error hover:bg-error/10 cursor-not-allowed',
+  ]"
+                        @click.prevent="queue.is_released_from_boned_area && router.visit(route('call-center.examination.create', queue.id))"
+                    >
+                        <template #content>
+                            <div class="flex justify-between items-center mb-6">
+                                <div class="text-2xl">
+                                    <i
+                                        v-if="queue.is_released_from_boned_area"
+                                        v-tooltip="'Released From Boned Area'"
+                                        class="ti ti-tag-starred mr-1 text-success"
+                                    ></i>
+                                    <i
+                                        v-else
+                                        v-tooltip="'Not Released From Boned Area'"
+                                        class="ti ti-tag-off mr-1 text-error"
+                                    ></i>
+                                </div>
+                                <div
+                                    :class="['flex items-center gap-2 ', queue.is_released_from_boned_area ? 'text-success' : 'text-error']">
+                                    <i class="ti ti-packages text-2xl"></i>
+                                    <span class="text-2xl font-semibold">{{ queue.package_count }}</span>
+                                </div>
+                            </div>
+
+                            <div class="text-center mb-6">
+                                <h1 class="text-5xl xl:text-[80px] font-extrabold text-gray-900 tracking-wide">
+                                    {{ queue?.token }}
+                                </h1>
+                            </div>
+
+                            <div class="flex flex-wrap justify-center gap-1">
+                                <Tag
+                                    :severity="queue.is_released_from_boned_area ? 'success' : 'danger'"
+                                    :value="queue.customer"
+                                    class="rounded-full px-4 py-2 text-base"
+                                />
+                            </div>
+                        </template>
+                    </Card>
+                </div>
+            </template>
+
+            <template #empty>
+                <div class="flex p-10 justify-center">
+                    No Tokens found.
+                </div>
+            </template>
+        </DataView>
     </AppLayout>
 </template>
