b6e670c13c836337b8221bb0d06e510550afa595
Ahinsa <isurujayawickramat@gmail.com>
2025-06-04 13:18:00 +0530

    LAC-825: HBL Overview UI Fixed

--- resources/js/Pages/Common/Components/InfoDisplay.vue
+++ resources/js/Pages/Common/Components/InfoDisplay.vue
@@ -12,11 +12,11 @@ defineProps({
 </script>
 
 <template>
-    <div>
-        <p class="text-xs uppercase text-slate-400 dark:text-navy-300">
+    <div class="flex flex-col">
+        <p class="text-xs uppercase text-slate-400 dark:text-navy-300 flex-shrink-0" style="width: 120px">
             {{ label }}
         </p>
-        <p class="mt-1 font-medium text-slate-700 dark:text-navy-100">
+        <p class="font-medium text-slate-700 dark:text-navy-100 break-words">
             {{ value || '-' }}
         </p>
     </div>
