770ab8558d43c44d2e1af10c1bcc92c5e8bf7a5f
Ahinsa <isurujayawickramat@gmail.com>
2025-07-17 10:33:45 +0530

    Whatsapp Issue

--- app/Http/Controllers/Api/v1/PickupController.php
+++ app/Http/Controllers/Api/v1/PickupController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api\v1;
 
+use App\Events\PickupCreated;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\v1\StorePickupExceptionRequest;
 use App\Http\Requests\StorePickupRequest;
@@ -39,7 +40,12 @@ class PickupController extends Controller
      */
     public function store(StorePickupRequest $request)
     {
-        return $this->pickupRepository->storePickup($request->all());
+        $response = $this->pickupRepository->storePickup($request->all());
+        $pickup = $response->getData();
+
+        PickupCreated::dispatch($pickup);
+
+        return $response;
     }
 
     /**
