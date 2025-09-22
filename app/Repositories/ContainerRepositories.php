90ca12444f3050076f70458de797c90da7f89afb
Tharusha <tharushawijerathna@yahoo.com>
2025-09-22 17:03:13 +0530

    LAC-957: Resloved Copilet Reviews

--- app/Repositories/ContainerRepositories.php
+++ app/Repositories/ContainerRepositories.php
@@ -50,6 +50,7 @@ use Dompdf\Dompdf;
 use Dompdf\Options;
 use Illuminate\Filesystem\Filesystem;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Log;
@@ -337,7 +338,7 @@ class ContainerRepositories implements ContainerRepositoryInterface, GridJsInter
     /**
      * Map packages with HBL data
      *
-     * @param array|App\Models\HBL $hbl
+     * @param array|HBL $hbl
      * @return array
      */
     private function mapPackagesWithHblData(array|HBL $hbl): array
@@ -345,7 +346,7 @@ class ContainerRepositories implements ContainerRepositoryInterface, GridJsInter
         // Convert HBL model to array if needed
         if ($hbl instanceof HBL) {
             $hbl = $hbl->toArray();
-            if (isset($hbl['packages']) && $hbl['packages'] instanceof \Illuminate\Database\Eloquent\Collection) {
+            if (isset($hbl['packages']) && $hbl['packages'] instanceof Collection) {
                 $hbl['packages'] = $hbl['packages']->toArray();
             }
         }
