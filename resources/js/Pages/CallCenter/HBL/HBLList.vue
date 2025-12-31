02be92ffc5bd96a7404847944a6e329c5f396a14
Ahinsa <isurujayawickramat@gmail.com>
2025-12-31 12:53:18 +0530

    Display Unloaded date Fixed

--- resources/js/Pages/CallCenter/HBL/HBLList.vue
+++ resources/js/Pages/CallCenter/HBL/HBLList.vue
@@ -33,10 +33,6 @@ const props = defineProps({
         type: Object,
         default: () => {},
     },
-    hbls: {
-        type: Object,
-        default: () => {},
-    },
     paymentStatus: {
         type: Object,
         default: () => {},
@@ -210,46 +206,46 @@ watch(() => filters.value.global.value, (newValue) => {
     }
 });
 
-watch(() => filters.value.warehouse.value, (newValue) => {
+watch(() => filters.value.warehouse.value, () => {
     fetchHBLs(1, filters.value.global.value);
 });
 
-watch(() => filters.value.hbl_type.value, (newValue) => {
+watch(() => filters.value.hbl_type.value, () => {
     fetchHBLs(1, filters.value.global.value);
 });
 
-watch(() => filters.value.cargo_type.value, (newValue) => {
+watch(() => filters.value.cargo_type.value, () => {
     fetchHBLs(1, filters.value.global.value);
 });
 
-watch(() => filters.value.is_hold.value, (newValue) => {
+watch(() => filters.value.is_hold.value, () => {
     fetchHBLs(1, filters.value.global.value);
 });
 
-watch(() => filters.value.user.value, (newValue) => {
+watch(() => filters.value.user.value, () => {
     fetchHBLs(1, filters.value.global.value);
 });
 
-watch(() => filters.value.payments.value, (newValue) => {
+watch(() => filters.value.payments.value, () => {
     fetchHBLs(1, filters.value.global.value);
 });
 
-watch(() => filters.value.shipment.value, (newValue) => {
+watch(() => filters.value.shipment.value, () => {
     fetchHBLs(1, filters.value.global.value);
 });
 
-watch(() => fromDate.value, (newValue) => {
+watch(() => fromDate.value, () => {
     fetchHBLs(1, filters.value.global.value);
 });
 
-watch(() => toDate.value, (newValue) => {
+watch(() => toDate.value, () => {
     fetchHBLs(1, filters.value.global.value);
 });
 
 const onPageChange = (event) => {
     perPage.value = event.rows;
     currentPage.value = event.page + 1;
-    fetchHBLs(currentPage.value);
+    fetchHBLs(currentPage.value, filters.value.global.value);
 };
 
 const onSort = (event) => {
@@ -328,7 +324,7 @@ const clearFilter = () => {
     };
     fromDate.value = moment(new Date()).subtract(24, "months").toISOString().split("T")[0];
     toDate.value = moment(new Date()).toISOString().split("T")[0];
-    fetchHBLs(currentPage.value);
+    fetchHBLs(1);
 };
 
 const confirmHBLDelete = (hbl) => {
