44454d08b32fe17c99bcc9321ff7eabdfbefe3f0
Ahinsa <isurujayawickramat@gmail.com>
2025-12-31 12:55:13 +0530

    Display Unloaded date Fixed

--- app/Actions/HBL/HBLPackage/MarkAsFullyUnloaded.php
+++ app/Actions/HBL/HBLPackage/MarkAsFullyUnloaded.php
@@ -15,6 +15,8 @@ class MarkAsFullyUnloaded
         $hbl_package = HBLPackage::withoutGlobalScope(BranchScope::class)->with('hbl')
             ->find($hbl_package_id);
         $hbl_package->is_unloaded = true;
+        $hbl_package->unloaded_at = now();
+        $hbl_package->unloaded_by = auth()->id();
         $hbl_package->current_warehouse = session('current_branch_id');
         if ($hbl_package->hbl->warehouse_id === session('current_branch_id')) {
             $hbl_package->is_de_unloaded = true;
