6f3c30bd8991ccb93c5d0a796e0644fe7e59deee
Ahinsa <isurujayawickramat@gmail.com>
2026-02-10 13:41:11 +0530

    LAC-1087: Invoice Issue

--- resources/views/pdf/cashier/invoice-v2.blade.php
+++ resources/views/pdf/cashier/invoice-v2.blade.php
@@ -17,7 +17,7 @@
                     <img src="{{$logoPath}}" alt="Logo" class="h-full w-full object-contain" />
                 </div>
                 <div class="flex-1">
-                    <div class="mb-1 text-right text-xs font-bold">CASHIER'S COPY</div>
+                    <div class="-mt-3 mb-1 text-right text-xs font-bold">CASHIER'S COPY</div>
                     <h1 class="text-sm font-bold leading-tight invisible">LAKSIRI INTERNATIONAL FREIGHT FORWARDERS (PVT) LTD.</h1>
                     <p class="text-xs font-bold invisible">U.P.B. Warehouse</p>
                     <p class="text-xs invisible">66, NEW NUGE ROAD,</p>
@@ -28,13 +28,13 @@
                 </div>
             </div>
 
-            <div class="-mt-16 flex justify-between text-xs">
+            <div class="-mt-10 flex justify-between text-xs">
                 <div><span class="invisible">Clearing Time : </span><span class="text-gray-800">{{ $data['clearing_time'] }}</span></div>
                 <div><span class="invisible">Vehicle No :</span></div>
             </div>
 
             <div class="flex justify-between text-xs">
-                <div><span class="invisible">Date : </span><span class="text-gray-800">{{ $data['date'] }}</span></div>
+                <div><span class="invisible">Date : </span><span class="text-gray-800 ml-2">{{ $data['date'] }}</span></div>
                 <div><span class="invisible">Serial No. : </span><span class="text-gray-800">{{ $data['hbl']['hbl_number'] ?? '' }}</span></div>
             </div>
 
@@ -44,7 +44,7 @@
             </div>
 
             <!-- Customer Details -->
-            <div class="mb-2 !space-y-3 !ml-7 text-xs">
+            <div class="mb-2 !space-y-3 !ml-7 text-xs mt-5">
                 <div class="flex">
                     <span class="invisible w-40">Name of Consignee</span>
                     <span class="text-gray-800">{{$data['hbl']['consignee_name']}}</span>
@@ -80,7 +80,7 @@
             </div>
 
             <!-- Charges Table -->
-            <div class="mb-2 !mt-28">
+            <div class="mb-2 !mt-23">
                 <table class="w-full border-collapse text-xs">
                     <thead>
                     <tr class="bg-gray-200">
@@ -137,7 +137,7 @@
             </div>
 
             <!-- Payment Summary -->
-            <div class="mb-2 text-xs">
+            <div class="mb-2 text-xs mt-4">
                 <div class="mb-1">
                     <span class="font-bold invisible">Rupees :</span>
                     <div class="ml-12">
@@ -203,7 +203,7 @@
             </div>
 
             <div class="mb-2 flex justify-between text-xs !ml-10">
-                <div><span class="invisible">Gate Pass : </span><span class="text-gray-800">0.00</span></div>
+{{--                <div><span class="invisible">Gate Pass : </span><span class="text-gray-800">0.00</span></div>--}}
             </div>
 
             <!-- Title -->
@@ -218,8 +218,8 @@
                     <span class="text-gray-800">{{ $data['hbl']['hbl_number'] ?? '' }}</span>
                 </div>
                 <div class="flex">
-                    <span class="w-40">Destuff Date.</span>
-                    <span class="text-gray-800">{{ $data['vessel']['unloading_ended_at'] ? \Carbon\Carbon::parse($data['vessel']['unloading_ended_at'])->format('Y-m-d') : '' }}</span>
+                    <span class="w-40 invisible">Destuff Date.</span>
+                    <span class="text-gray-800 invisible">{{ $data['vessel']['unloading_ended_at'] ? \Carbon\Carbon::parse($data['vessel']['unloading_ended_at'])->format('Y-m-d') : '' }}</span>
                 </div>
                 <div class="flex">
                     <span class="invisible w-40">B/L No.</span>
@@ -291,7 +291,7 @@
                     <span class="invisible w-32">Serial No.</span>
                     <span class="text-gray-800"></span>
                 </div>
-                <div class="flex">
+                <div class="flex mt-6 ml-10">
                     <span class="invisible w-32">User ID</span>
                     <span class="text-gray-800">{{ strtoupper($data['by']) }}</span>
                 </div>
