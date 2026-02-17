4e588e0cf941d2ca3f87bd5569c26e598155bb8f
Ahinsa <isurujayawickramat@gmail.com>
2026-02-17 14:48:50 +0530

    LAC-1096-1: WIP

--- resources/views/pdf/cashier/invoice-v3.blade.php
+++ resources/views/pdf/cashier/invoice-v3.blade.php
@@ -6,10 +6,10 @@
     <title>Warehouse Invoice - Sea Cargo</title>
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
-<body class="text-gray-800">
+<body class="text-gray-800 text-xs">
 
 {{-- CASHIER'S COPY --}}
-<div style="position: absolute; top: 1.2cm; left: 8.5cm;">CASHIER'S COPY</div>
+<div style="position: absolute; top: 1.2cm; left: 8cm;" class="text-lg">CASHIER'S COPY</div>
 
 {{-- Clearing Time --}}
 <div style="position: absolute; top: 4.2cm; left: 4.2cm;"><span>Clearing Time : </span>
@@ -17,69 +17,69 @@
 </div>
 
 {{-- Date --}}
-<div style="position: absolute; top: 5.1cm; left: 2.8cm;">
+<div style="position: absolute; top: 4.9cm; left: 2cm;">
     {{ \Carbon\Carbon::today()->toDateString()}}
 </div>
 
 {{-- Serial Number --}}
-<div style="position: absolute; top: 5cm; left: 8.5cm;">{{ $data['hbl']['hbl_number'] ?? '' }}</div>
+<div style="position: absolute; top: 4.9cm; left: 8cm;">{{ $data['hbl']['hbl_number'] ?? '' }}</div>
 
 {{-- Name of Consignee --}}
-<div style="position: absolute; top: 7.4cm; left: 6.1cm;">{{$data['hbl']['consignee_name']}}</div>
+<div style="position: absolute; top: 7.4cm; left: 5cm;">{{$data['hbl']['consignee_name']}}</div>
 
 {{-- B/L No --}}
-<div style="position: absolute; top: 8.3cm; left: 6.1cm;">{{ $data['vessel']['bl_number'] ?? ''}}</div>
+<div style="position: absolute; top: 8.3cm; left: 5cm;">{{ $data['vessel']['bl_number'] ?? ''}}</div>
 
 {{-- Vessel --}}
-<div style="position: absolute; top: 9.2cm; left: 6.1cm;">{{ $data['vessel']['vessel_name'] ?? '' }}</div>
+<div style="position: absolute; top: 9.2cm; left: 5cm;">{{ $data['vessel']['vessel_name'] ?? '' }}</div>
 
 {{-- Passport / I.D. Card No --}}
-<div style="position: absolute; top: 10.3cm; left: 6.1cm;">{{$data['hbl']['nic']}}</div>
+<div style="position: absolute; top: 10.3cm; left: 5cm;">{{$data['hbl']['nic']}}</div>
 
 {{-- >No. of Packages --}}
-<div style="position: absolute; top: 11.1cm; left: 6.1cm;">{{ count($data['hbl']['packages']) }}</div>
+<div style="position: absolute; top: 11.1cm; left: 5cm;">{{ count($data['hbl']['packages']) }}</div>
 
 {{-- Bond Storage No. --}}
-<div style="position: absolute; top: 12cm; left: 6.1cm;">@if(!empty($data['bond_storage_numbers'])){{ implode(', ', $data['bond_storage_numbers']) }}@else N/A @endif</div>
+<div style="position: absolute; top: 11.8cm; left: 5cm;">@if(!empty($data['bond_storage_numbers'])){{ implode(', ', $data['bond_storage_numbers']) }}@else N/A @endif</div>
 
 {{-- Agent --}}
-<div style="position: absolute; top: 12.5cm; left: 6.1cm;">{{ $data['hbl']['branch']['name'] }}</div>
+<div style="position: absolute; top: 12.5cm; left: 5cm;">{{ $data['hbl']['branch']['name'] }}</div>
 
 {{-- Volume --}}
-<div style="position: absolute; top: 13.7cm; left: 6.1cm;">{{ $data['grand_volume'] }}</div>
+<div style="position: absolute; top: 13.5cm; left: 5cm;">{{ $data['grand_volume'] }}</div>
 
 {{-- Table --}}
 {{-- Sri Lanka Port Charges - Sea Cargo --}}
-<div style="position: absolute; top: 15cm; left: 8.5cm;">{{ number_format($data['charges']['port_charge']['rate'],2) }}</div>
-<div style="position: absolute; top: 15cm; left: 10.2cm;">{{ number_format($data['charges']['port_charge']['amount'],2) }}</div>
+<div style="position: absolute; top: 15cm; left: 7cm;">{{ number_format($data['charges']['port_charge']['rate'],2) }}</div>
+<div style="position: absolute; top: 15cm; left: 9cm;">{{ number_format($data['charges']['port_charge']['amount'],2) }}</div>
 
-<div style="position: absolute; top: 15.9cm; left: 8.5cm;">0.00</div>
-<div style="position: absolute; top: 15.9cm; left: 10.2cm;">0.00</div>
+<div style="position: absolute; top: 15.9cm; left: 7cm;">0.00</div>
+<div style="position: absolute; top: 15.9cm; left: 9cm;">0.00</div>
 
 {{-- Handling Charges ( Per Package ) --}}
-<div style="position: absolute; top: 16.5cm; left: 8.5cm;">{{ number_format($data['charges']['handling_charge']['rate'],2) }}</div>
-<div style="position: absolute; top: 16.5cm; left: 10.2cm;">{{ number_format($data['charges']['handling_charge']['amount'],2) }}</div>
+<div style="position: absolute; top: 16.5cm; left: 7cm;">{{ number_format($data['charges']['handling_charge']['rate'],2) }}</div>
+<div style="position: absolute; top: 16.5cm; left: 9cm;">{{ number_format($data['charges']['handling_charge']['amount'],2) }}</div>
 
 {{-- Bond Storage Charges ( Per Cubic Foot ) --}}
-<div style="position: absolute; top: 17.4cm; left: 8.5cm;">{{ number_format($data['charges']['storage_charge']['rate'],2) }}</div>
-<div style="position: absolute; top: 17.4cm; left: 10.2cm;">{{ number_format($data['charges']['storage_charge']['amount'],2) }}</div>
+<div style="position: absolute; top: 17.4cm; left: 7cm;">{{ number_format($data['charges']['storage_charge']['rate'],2) }}</div>
+<div style="position: absolute; top: 17.4cm; left: 9cm;">{{ number_format($data['charges']['storage_charge']['amount'],2) }}</div>
 
 {{-- Demurrage Charges ( Per Cubic Foot ) --}}
-<div style="position: absolute; top: 19.1cm; left: 8.5cm;">9.50</div>
-<div style="position: absolute; top: 19.7cm; left: 8.5cm;">10.00</div>
+<div style="position: absolute; top: 19.1cm; left: 7cm;">9.50</div>
+<div style="position: absolute; top: 19.7cm; left: 7cm;">10.00</div>
 
-<div style="position: absolute; top: 19.4cm; left: 10.2cm;">{{ number_format($data['charges']['dmg_charge']['amount'],2) }}</div>
+<div style="position: absolute; top: 19.4cm; left: 9cm;">{{ number_format($data['charges']['dmg_charge']['amount'],2) }}</div>
 
 {{-- TOTAL --}}
-<div style="position: absolute; top: 20cm; left: 10.2cm;">{{ number_format($data['charges']['total'],2)}}</div>
+<div style="position: absolute; top: 20cm; left: 9cm;" class="font-bold text-lg">{{ number_format($data['charges']['total'],2)}}</div>
 
 
 <!-- Payment Summary -->
-<div style="position: absolute; top: 21cm; left: 4.9cm;"><span>D/O Charges. </span><span>{{ number_format($data['charges']['do_charge'],2) }}</span></div>
+<div style="position: absolute; top: 21.2cm; left: 4cm;"><span>D/O Charges. </span><span>{{ number_format($data['charges']['do_charge'],2) }}</span></div>
 
-<div style="position: absolute; top: 22cm; left: 4.9cm;"><span>Stamp Duty </span><span>{{ number_format($data['charges']['stamp_charge'],2) }}</span></div>
+<div style="position: absolute; top: 21.7cm; left: 4cm;"><span>Stamp Duty </span><span>{{ number_format($data['charges']['stamp_charge'],2) }}</span></div>
 
-<div style="position: absolute; top: 23.5cm; left: 4.9cm;" class="font-bold"><span>G/Total : </span><span>{{ number_format($data['charges']['total']+$data['charges']['do_charge']+$data['charges']['stamp_charge'],2) }}</span></div>
+<div style="position: absolute; top: 22.3cm; left: 4cm;" class="font-bold"><span>G/Total : </span><span>{{ number_format($data['charges']['total']+$data['charges']['do_charge']+$data['charges']['stamp_charge'],2) }}</span></div>
 
 @if (!empty($data['taxes']))
     <div style="position: absolute; top: 24.5cm; left: 1.8cm;">VAT 18% included in the charges</div><br />
